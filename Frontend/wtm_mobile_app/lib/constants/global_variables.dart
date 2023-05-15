34159f979e4c09730c5f3defd48889a301ff6eea
Lissetx <98356241+Lissetx@users.noreply.github.com>
2023-05-15 02:31:46 -0600

    homepage started

---
+++ Frontend/wtm_mobile_app/lib/constants/global_variables.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class GlobalVariables{
+  static const appBarGradient = LinearGradient(
+    colors: [
+      Color.fromARGB(255, 235, 63, 60),
+      Color.fromARGB(255, 159, 15, 15),
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+  static const backgroundColor = Color.fromARGB(255, 123, 123, 123);
+}
---
+++ Frontend/wtm_mobile_app/lib/features/auth/screens/auth_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget{
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen>{
+  
+}
+
